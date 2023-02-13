04505824155ce1a83982f1ee7b8453060efaddd6
hebaomar94 <hebaomar93.ho@gmail.com>
2023-02-13 21:44:11 +0000

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Contains the class TestConsoleDocs
 """
@@ -8,18 +7,13 @@ import console
 import inspect
 import pep8
 import unittest
-
 HBNBCommand = console.HBNBCommand
 
 
 class TestConsoleDocs(unittest.TestCase):
-
     """Class for testing documentation of the console"""
-
     def test_pep8_conformance_console(self):
-
         """Test that console.py conforms to PEP8."""
-
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['console.py'])
         self.assertEqual(result.total_errors, 0,
@@ -40,9 +34,7 @@ class TestConsoleDocs(unittest.TestCase):
                         "console.py needs a docstring")
 
     def test_HBNBCommand_class_docstring(self):
-
         """Test for the HBNBCommand class docstring"""
-
         self.assertIsNot(HBNBCommand.__doc__, None,
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
